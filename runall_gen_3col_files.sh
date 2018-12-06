9d46fb723ac736aca50f6439fbed30fb044ec9b3
DVSneuro <david.v.smith@temple.edu>
2018-12-06 17:11:37 -0500

    initial commit; copying from srndna repo

---
+++ runall_gen_3col_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#for subrun in "104 5" "105 5" "106 3" "107 5" "108 5" "109 2" "110 2" "112 5" "113 5"; do
+for subrun in "111 5" "115 5" "116 5" "117 5" "118 5" "120 5" "121 5"; do
+
+  set -- $subrun
+  sub=$1
+  run=$2
+  echo "$sub $run"
+
+	#Manages the number of jobs and cores
+	SCRIPTNAME=gen_3col_files.sh
+	NCORES=12
+	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+  		sleep 1m
+	done
+	bash $SCRIPTNAME $sub $run
+	sleep 5s
+
+done
